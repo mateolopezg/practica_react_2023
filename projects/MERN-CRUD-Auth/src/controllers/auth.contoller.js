23cc7a9827f61481cc1f7f399c73e1df1e9a280e
jilguerillo <mate_lg@tutanota.com>
2023-12-09 22:45:27 -0300

    feature: implements JWT

--- projects/MERN-CRUD-Auth/src/controllers/auth.contoller.js
+++ projects/MERN-CRUD-Auth/src/controllers/auth.contoller.js
@@ -1,18 +1,30 @@
 import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
+import { createAccessToken } from "../libs/jwt.js";
 
 export const register = async (req, res) => {
   const { email, password, username } = req.body
 
   try {
+    const hash = await bcrypt.hash(password, 10)
+
     const newUser = new User({
       username,
-      password,
+      password: hash,
       email
     })
 
     const userSaved = await newUser.save()
-    console.log(email, password, username)
-    res.json(userSaved)
+    const token = await createAccessToken({ id: userSaved._id })
+
+    res.cookie('token', token)
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt
+    })
   } catch (error) {
     console.error(error)
   }
